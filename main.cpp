b37ee41fed2104663cfee51bb8a3eda08dd40191
Microhard_1724 <122979224+microharddemo@users.noreply.github.com>
2023-02-12 11:34:00 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<windows.h>//头文件
+using namespace std;
+int main()//主函数
+{
+	while(true)//死循环代码
+	{
+		cout << "\a"; //输出转义字符\a代表响铃
+		Sleep(1000);//停止1秒等待响完
+	}
+	return 0;
+}
